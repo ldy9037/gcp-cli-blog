c23b422f3c4bedf6f9ee7a2fd3baacc4f4d549b0
vntg-ldy <ldy@vntgcorp.com>
2022-02-17 22:19:58 +0900

    http health check 추가

--- compute/mig-cli.sh
+++ compute/mig-cli.sh
@@ -15,3 +15,9 @@ gcloud compute firewall-rules create allow-http-ingress-from-healthcheck \
     --direction ingress \
     --rules tcp:80 \
     --source-ranges 130.211.0.0/22,35.191.0.0/16
+
+# 자동 복구 기능을 사용하기 위해 health check 생성
+gcloud compute health-checks create http http-health-check \
+    --request-path=/ \
+    --check-interval=30s \
+    --port 80
