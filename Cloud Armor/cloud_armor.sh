1debaa6b90fbef82c41690b6afcf9632ec7a702b
emadam <ldy9037@naver.com>
2024-06-29 21:39:21 +0900

    feat: IP 차단 보안 규칙 추가

--- Cloud Armor/cloud_armor.sh
+++ Cloud Armor/cloud_armor.sh
@@ -15,3 +15,9 @@ gcloud compute backend-services update emadam-test-backend-service \
 # 백엔드 서비스에 연결된 보안 정책 확인
 gcloud compute backend-services describe emadam-test-backend-service \
     --global | grep securityPolicy | awk '{ print $2 }'
+
+# IP 차단 규칙 생성
+gcloud compute security-policies rules create 1 \
+    --security-policy emadam-test-security-policy \
+    --src-ip-ranges "211.243.179.113" \
+    --action "deny-403"
