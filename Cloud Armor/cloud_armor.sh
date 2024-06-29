9fa720aec9871d4b8e90b6948d3149c6a9b4aa89
emadam <ldy9037@naver.com>
2024-06-29 21:12:17 +0900

    feat: 백엔드 보안 정책 생성

---
+++ Cloud Armor/cloud_armor.sh
@@ -0,0 +1,17 @@
+# 백엔드 보안 정책 생성
+gcloud compute security-policies create emadam-test-security-policy \
+    --type=CLOUD_ARMOR \
+    --global
+
+# 백엔드 보안 정책 확인
+gcloud compute security-policies describe emadam-test-security-policy \
+    --global
+
+# 백엔드 서비스에 보안 정책 연결
+gcloud compute backend-services update emadam-test-backend-service \
+    --security-policy=emadam-test-security-policy \
+    --global
+
+# 백엔드 서비스에 연결된 보안 정책 확인
+gcloud compute backend-services describe emadam-test-backend-service \
+    --global | grep securityPolicy | awk '{ print $2 }'
